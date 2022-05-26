42df75c547bb13a52fd99c0d6415aacb013d31a9
tamuten <tamuten310@gmail.com>
2022-05-26 22:28:31 +0900

    CalendarServiceTestのparse部分を変更

--- src/test/java/com/example/demo/domain/service/CalendarServiceTest.java
+++ src/test/java/com/example/demo/domain/service/CalendarServiceTest.java
@@ -1,25 +1,23 @@
 package com.example.demo.domain.service;
 
-import static com.example.demo.util.LocalDateParser.toLocalDate;
-import static com.example.demo.util.calendar.CalendarService.generate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import com.example.demo.util.calendar.CalendarService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CalendarServiceTest {
-  private static final String FORMAT_DATE_SLASH = "yyyy/MM/dd";
-
   @Test
   void generateは対象日をメイン月とするカレンダーを作成する() {
     // Setup
     List<LocalDate> expected = CalendarServiceTestHelper.generateExpectedList();
 
-    List<LocalDate> actual = generate(toLocalDate("2022/03/30", FORMAT_DATE_SLASH));
+    List<LocalDate> actual = CalendarService.generate(LocalDate.parse("2022-03-30"));
 
     assertThat(actual.size()).isEqualTo(expected.size());
     assertThat(actual).isEqualTo(expected);
--- src/test/java/com/example/demo/domain/service/CalendarServiceTestHelper.java
+++ src/test/java/com/example/demo/domain/service/CalendarServiceTestHelper.java
@@ -1,51 +1,47 @@
 package com.example.demo.domain.service;
 
-import static com.example.demo.util.LocalDateParser.toLocalDate;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CalendarServiceTestHelper {
-  private static final String FORMAT_DATE_SLASH = "yyyy/MM/dd";
-
   public static List<LocalDate> generateExpectedList() {
     List<LocalDate> expectedList = new ArrayList<>();
-    expectedList.add(toLocalDate("2022/02/27", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/02/28", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/01", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/02", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/03", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/04", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/05", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/06", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/07", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/08", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/09", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/10", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/11", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/12", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/13", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/14", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/15", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/16", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/17", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/18", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/19", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/20", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/21", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/22", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/23", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/24", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/25", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/26", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/27", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/28", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/29", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/30", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/03/31", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/04/01", FORMAT_DATE_SLASH));
-    expectedList.add(toLocalDate("2022/04/02", FORMAT_DATE_SLASH));
+    expectedList.add(LocalDate.parse("2022-02-27"));
+    expectedList.add(LocalDate.parse("2022-02-28"));
+    expectedList.add(LocalDate.parse("2022-03-01"));
+    expectedList.add(LocalDate.parse("2022-03-02"));
+    expectedList.add(LocalDate.parse("2022-03-03"));
+    expectedList.add(LocalDate.parse("2022-03-04"));
+    expectedList.add(LocalDate.parse("2022-03-05"));
+    expectedList.add(LocalDate.parse("2022-03-06"));
+    expectedList.add(LocalDate.parse("2022-03-07"));
+    expectedList.add(LocalDate.parse("2022-03-08"));
+    expectedList.add(LocalDate.parse("2022-03-09"));
+    expectedList.add(LocalDate.parse("2022-03-10"));
+    expectedList.add(LocalDate.parse("2022-03-11"));
+    expectedList.add(LocalDate.parse("2022-03-12"));
+    expectedList.add(LocalDate.parse("2022-03-13"));
+    expectedList.add(LocalDate.parse("2022-03-14"));
+    expectedList.add(LocalDate.parse("2022-03-15"));
+    expectedList.add(LocalDate.parse("2022-03-16"));
+    expectedList.add(LocalDate.parse("2022-03-17"));
+    expectedList.add(LocalDate.parse("2022-03-18"));
+    expectedList.add(LocalDate.parse("2022-03-19"));
+    expectedList.add(LocalDate.parse("2022-03-20"));
+    expectedList.add(LocalDate.parse("2022-03-21"));
+    expectedList.add(LocalDate.parse("2022-03-22"));
+    expectedList.add(LocalDate.parse("2022-03-23"));
+    expectedList.add(LocalDate.parse("2022-03-24"));
+    expectedList.add(LocalDate.parse("2022-03-25"));
+    expectedList.add(LocalDate.parse("2022-03-26"));
+    expectedList.add(LocalDate.parse("2022-03-27"));
+    expectedList.add(LocalDate.parse("2022-03-28"));
+    expectedList.add(LocalDate.parse("2022-03-29"));
+    expectedList.add(LocalDate.parse("2022-03-30"));
+    expectedList.add(LocalDate.parse("2022-03-31"));
+    expectedList.add(LocalDate.parse("2022-04-01"));
+    expectedList.add(LocalDate.parse("2022-04-02"));
 
     return expectedList;
   }
